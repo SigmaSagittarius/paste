fec6004fda884117f2f3dbff8efd1a5ac1337c2c
SigmaSagittarius <112798770+SigmaSagittarius@users.noreply.github.com>
2022-11-01 19:47:27 +0800

    Update 跳跃计算.cpp

--- IWC/跳跃计算.cpp
+++ IWC/跳跃计算.cpp
@@ -11,3 +11,7 @@ namespace iwbtc {
    Kid next(Kid x)   {x.speed=x.speed-Frac{29,100};if(Frac{-829,100}>x.speed)x.speed=Frac{-829,100};x.pos=x.pos+(x.speed*Frac{1,50});return x;}
    Kid cancle(Kid x) {if(x.speed>Frac{0,1}) x.speed=x.speed*Frac{9,20};return x;}
 }
+/*
+请注意在 IWC 中一帧可以解除多次，这在 IW 里是不行的。
+而且我没有添加落地碰撞计算，因为落地碰撞计算在 IWC 中尚不明确计算原理。
+*/
