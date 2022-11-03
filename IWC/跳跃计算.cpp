1e476c3f58d93d8a901f61f054ac4852794c6d22
SigmaSagittarius <112798770+SigmaSagittarius@users.noreply.github.com>
2022-11-03 15:47:39 +0800

    Update 跳跃计算.cpp

--- IWC/跳跃计算.cpp
+++ IWC/跳跃计算.cpp
@@ -8,7 +8,7 @@ namespace iwbtc {
    bool operator >(Frac a,Frac b) {if(sign(a)^sign(b))return sign(a)>sign(b);return (sign(a)==-1)^(abs(a.a*b.b)>abs(b.a*a.b));}
    double Double(Frac a) {return double(a.a)/a.b;}
    void show(Kid x)  {cerr << fixed << setprecision(10) << "pos:" << Double(x.pos) << " speed:" << Double(x.speed) << endl;}
-   Kid next(Kid x)   {x.speed=x.speed-Frac{29,100};if(Frac{-829,100}>x.speed)x.speed=Frac{-829,100};x.pos=x.pos+(x.speed*Frac{1,50});return x;}
+   Kid next(Kid x)   {x.pos=x.pos+(x.speed*Frac{1,50});x.speed=x.speed-Frac{29,100};if(Frac{-829,100}>x.speed)x.speed=Frac{-829,100};return x;}
    Kid cancle(Kid x) {if(x.speed>Frac{0,1}) x.speed=x.speed*Frac{9,20};return x;}
 }
 /*
